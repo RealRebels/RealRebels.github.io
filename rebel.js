d8e6091b23d2360549bd5312318e413b8d4a8609
RealRebels <131822998+RealRebels@users.noreply.github.com>
2023-04-25 17:41:50 -0400

    Add files via upload

---
+++ rebel.js
@@ -0,0 +1,16 @@
+
+
+function bagThis() {
+    var bagged = document.getElementById("sub-header");
+    bagged.innerHTML = "BAG DIS DI*K!!!"
+}
+
+function bootleg() {
+    const speech = new Audio();
+    speech.src = "./BootLegGreg.mp3";
+    speech.play();
+}
+
+function returner() {
+    alert("THANK YOU PATRIOT!!");
+}
